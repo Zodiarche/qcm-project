2fc69b69573dc7a33393a96941076e52ec88ad00
Zøzo <benbar2412@gmail.com>
2025-02-19 10:31:27 +0100

    feat: Ajout nodemon + pnpm backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import dotenv from "dotenv";
-import { Resend } from "resend";
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import { Resend } from 'resend';
 
 dotenv.config();
 
@@ -13,16 +13,15 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-console.log("Resend API Key:", process.env.RESEND_API_KEY);
 
-app.post("/send-email", async (req, res) => {
+app.post('/send-email', async (req, res) => {
   const { name, firstname, email, message } = req.body;
 
   try {
     const response = await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: 'onboarding@resend.dev',
       to: email,
-      subject: "Hello World",
+      subject: 'Hello World',
       html: `
         <h>Nouveau message reçu</h>
         <p><strong>Nom :</strong> ${name} ${firstname}</p>
@@ -33,8 +32,8 @@ app.post("/send-email", async (req, res) => {
 
     res.status(200).json({ success: true, response });
   } catch (error) {
-    console.error("Erreur lors de l’envoi du mail:", error);
-    res.status(500).json({ error: "Erreur lors de l’envoi du mail" });
+    console.error('Erreur lors de l’envoi du mail:', error);
+    res.status(500).json({ error: 'Erreur lors de l’envoi du mail' });
   }
 });
 
