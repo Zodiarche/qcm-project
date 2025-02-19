10abd360d5ea2e233f1b1f4f34ba2fd9128235e6
Zøzo <benbar2412@gmail.com>
2025-02-19 10:26:26 +0100

    fix: Redirection sur GitHub dans un nouvel onglet

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,14 +1,12 @@
-import { Link } from 'react-router';
-
 const Footer = () => {
   return (
     <footer id="footer" className="bg-background border-t shadow-md">
       <div className="container mx-auto flex items-center justify-center px-6 py-4">
         <p className="text-sm">
           © {new Date().getFullYear()} -{' '}
-          <Link to="https://github.com/Zodiarche" className="text-foreground hover:text-primary transition">
+          <a href="https://github.com/Zodiarche" target="_blank" rel="noopener noreferrer" className="text-foreground hover:text-primary transition">
             Benjamin GUILLEMIN
-          </Link>
+          </a>
         </p>
       </div>
     </footer>
