6a01ba53404a3fad52a606dfb52207eeb1276215
Zøzo <benbar2412@gmail.com>
2025-02-19 10:21:52 +0100

    feat: Ajout Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 
 import Header from '@/components/Header';
-// import Footer from '@/components/Footer';
+import Footer from '@/components/Footer';
 import Home from '@/pages/Home';
 import Contact from '@/pages/Contact';
 
@@ -16,14 +16,16 @@ const App = () => {
       <Router>
         <div id="wrapper">
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/login" element={<div>Page de connexion</div>} />
-          </Routes>
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/login" element={<div>Page de connexion</div>} />
+            </Routes>
+          </main>
         </div>
 
-        {/* <Footer /> */}
+        <Footer />
       </Router>
     </QueryClientProvider>
   );
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { Link } from 'react-router';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer id="footer" className="bg-background border-t shadow-md">
+      <div className="container mx-auto flex items-center justify-center px-6 py-4">
+        <p className="text-sm">
+          © {new Date().getFullYear()} -{' '}
+          <Link to="https://github.com/Zodiarche" className="text-foreground hover:text-primary transition">
+            Benjamin GUILLEMIN
+          </Link>
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
